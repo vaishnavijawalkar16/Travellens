a3b861755848f4f38dfb32b0c29e326dceeb53b3
Vaishnavi Jawalkar <jawalkarvaishnavi604@gmail.com>
2026-04-02 15:46:36 +0530

    updated version

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/travellens";
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
